7c93dc45e964de864e0a08a10adc8a6e4ed6a6b6
ClockZinc <clockzinc@163.com>
2023-02-25 22:37:40 +0800

    SAC2

--- SAC_tf_github/myfun.py
+++ SAC_tf_github/myfun.py
@@ -138,3 +138,114 @@ def create_video(filename,video_name,env, actor, fps=30,max_step=10000):
                 break
             else:
                 max_step-=1
+
+
+
+
+class SumTree(object):
+    data_pointer = 0
+
+    def __init__(self, capacity):
+        self.capacity = capacity  # for all priority values
+        self.tree = np.zeros(2 * capacity - 1)
+        # [--------------Parent nodes-------------][-------leaves to recode priority-------]
+        #             size: capacity - 1                       size: capacity
+        self.data = np.zeros(capacity, dtype=object)  # for all transitions
+        # [--------------data frame-------------]
+        #             size: capacity
+        self.Full_flag=False
+
+    def add(self, p, data):
+        self.tree_idx = self.data_pointer + self.capacity - 1
+        self.data[self.data_pointer] = data  # update data_frame
+        self.update(self.tree_idx, p)  # update tree_frame
+
+        self.data_pointer += 1
+        if self.data_pointer >= self.capacity:  # replace when exceed the capacity
+            self.data_pointer = 0
+            self.Full_flag=True
+
+    def update(self, tree_idx, p):
+        change = p - self.tree[tree_idx]
+        self.tree[tree_idx] = p
+        # then propagate the change through tree
+        while tree_idx != 0:    # this method is faster than the recursive loop in the reference code
+            tree_idx = (tree_idx - 1) // 2
+            self.tree[tree_idx] += change
+
+    def get_leaf(self, v):
+        parent_idx = 0
+        while True:     # the while loop is faster than the method in the reference code
+            cl_idx = 2 * parent_idx + 1         # this leaf's left and right kids
+            cr_idx = cl_idx + 1
+            if cl_idx >= len(self.tree):        # reach bottom, end search
+                leaf_idx = parent_idx
+                break
+            else:       # downward search, always search for a higher priority node
+                if v <= self.tree[cl_idx]:
+                    parent_idx = cl_idx
+                else:
+                    v -= self.tree[cl_idx]
+                    parent_idx = cr_idx
+
+        data_idx = leaf_idx - self.capacity + 1
+        return leaf_idx, self.tree[leaf_idx], self.data[data_idx]
+
+    def total_p(self):
+        return self.tree[0]  # the root
+    
+class Memory_V1(object):  # stored as ( s, a, r, s_ ) in SumTree
+    """
+    This Memory class is modified based on the original code from:
+    https://github.com/jaara/AI-blog/blob/master/Seaquest-DDQN-PER.py
+    """
+
+
+    def __init__(self, capacity,s_dim,a_dim,epsilon = 0.01,alpha = 0.6,beta = 0.4,beta_increment_per_sampling = 0.0001,abs_err_upper = 1.):
+        self.tree = SumTree(capacity)
+        self.s_dim=s_dim
+        self.a_dim=a_dim
+        self.epsilon = epsilon  # small amount to avoid zero priority
+        self.alpha = alpha  # [0~1] convert the importance of TD error to priority
+        self.beta = beta  # importance-sampling, from initial value increasing to 1
+        self.beta_increment_per_sampling = beta_increment_per_sampling
+        self.abs_err_upper = abs_err_upper # clipped abs error
+        self.tree_size=2 * capacity - 1
+
+    def store(self,s,a,r,s_,done):
+        transition=np.hstack((s,a,r,s_,done)).astype(np.float32)# 横向堆叠这些变量
+        max_p = np.max(self.tree.tree[-self.tree.capacity:])
+        if max_p == 0:
+            max_p = self.abs_err_upper
+        self.tree.add(max_p, transition)   # set the max p for new p
+
+    def sample(self, n):
+        b_idx, b_memory, ISWeights = np.empty((n,), dtype=np.int32), np.empty((n, self.tree.data[0].size),np.float32), np.empty((n, 1),dtype=np.float32)
+        pri_seg = self.tree.total_p() / n       # priority segment
+        self.beta = np.min([1., self.beta + self.beta_increment_per_sampling])  # max = 1
+        if (not self.tree.Full_flag): # 如果没满的话
+            last_idx=self.tree.tree_idx# 最后一个的index就是确定的最后新加进去的
+        else:
+            last_idx=self.tree_size
+        min_prob = np.min(self.tree.tree[-self.tree.capacity:last_idx]) / self.tree.total_p() # for later calculate ISweight
+        for i in range(n):
+            a, b = pri_seg * i, pri_seg * (i + 1)
+            v = np.random.uniform(a, b)
+            idx, p, data = self.tree.get_leaf(v)
+            prob = p / self.tree.total_p()
+            ISWeights[i, 0] = np.power(prob/min_prob, -self.beta)
+            b_idx[i], b_memory[i, :] = idx, data
+
+        bs = b_memory[:, :self.s_dim]                         #从bt获得数据s
+        ba = b_memory[:, self.s_dim:self.s_dim + self.a_dim]  #从bt获得数据a
+        br = b_memory[:, -self.s_dim - 2:-self.s_dim-1]         #从bt获得数据r
+        bs_ = b_memory[:, -self.s_dim-1:-1]
+        bd = b_memory[:, -1:]
+        return bs,ba,br,bs_,bd,b_idx,ISWeights
+
+    def batch_update(self, tree_idx, abs_errors):
+        abs_errors += self.epsilon  # convert to abs and avoid 0
+        clipped_errors = np.minimum(abs_errors, self.abs_err_upper)
+        ps = np.power(clipped_errors, self.alpha)
+        for ti, p in zip(tree_idx, ps):
+            self.tree.update(ti, p)
